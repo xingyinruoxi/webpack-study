82ee227a631a466c3252dc3fc0fe457c13f1f109
周林 <634023699@qq.com>
2020-09-08 21:56:35 +0800

    添加配置

--- webpack.cofig.base.js
+++ webpack.cofig.base.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-    entry: './src/main.js',
+    entry: "./src/main.js",
     resolve: {
         //查找第三方依赖
         modules: [path.resolve(__dirname, "./node_modules")],
@@ -14,46 +14,40 @@ module.exports = {
         extensions: [".vue", ".js"],
     },
     module: {
-        rules: [
-            /*  {
-                 test: /\.(ttf|eot|woff|woff2|svg)$/,
-                 // exclude: /node_modules/,
-                 include: /node_modules/,
-                 use: {
-                     loader: 'file-loader',
-                     options: {
-                         name: './fonts/[name]_[hash:8].[ext]',
-                     }
-                 }
-             }, */
+        rules: [{
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                exclude: /node_modules/,
+                //  include: /node_modules/,
+                use: {
+                    loader: "file-loader",
+                    /*   options: {
+                                   name: './fonts/[name]_[hash:8].[ext]',
+                               } */
+                },
+            },
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, "./src"),
                 use: {
                     loader: "babel-loader",
-                }
+                },
             },
             {
                 test: /\.vue$/,
                 exclude: /node_modules/,
-                loader: 'vue-loader'
+                loader: "vue-loader",
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
                 include: path.resolve(__dirname, "./src"),
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
                     limit: 200,
-                    name: '[name]_[hash:8].[ext]',
-                    outputPath: './images'
-
-                }
+                    name: "[name]_[hash:8].[ext]",
+                    outputPath: "./images",
+                },
             },
-
-        ]
+        ],
     },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new VueLoaderPlugin()
-    ]
-}
+    plugins: [new CleanWebpackPlugin(), new VueLoaderPlugin()],
+};
