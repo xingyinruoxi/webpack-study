3d079959db1766f76d703d26a2f2049aae601953
周林 <634023699@qq.com>
2020-09-08 10:47:27 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
-  <div class="app">
-   Hellow,world!!!!{{msg}}
-   <img :src="pic" @click="picClickHandle">
-   <p>
-       <i class="fa fa-camera-retro fa-lg"></i> fa-lg
-<i class="fa fa-camera-retro fa-2x"></i> fa-2x
-<i class="fa fa-camera-retro fa-3x"></i> fa-3x
-<i class="fa fa-camera-retro fa-4x"></i> fa-4x
-<i class="fa fa-camera-retro fa-5x"></i> fa-5x
-   </p>
-  </div>
+<div class="app">
+    我是周林！！！！{{msg}}
+    <img :src="pic" @click="picClickHandle">
+    <p>
+        <i class="fa fa-camera-retro fa-lg"></i> fa-lg
+        <i class="fa fa-camera-retro fa-2x"></i> fa-2x
+        <i class="fa fa-camera-retro fa-3x"></i> fa-3x
+        <i class="fa fa-camera-retro fa-4x"></i> fa-4x
+        <i class="fa fa-camera-retro fa-5x"></i> fa-5x
+    </p>
+</div>
 </template>
+
 <script>
 import pic from './333.png'
 export default {
-    data(){
+    data() {
         return {
-            msg:'msg',
+            msg: 'msg',
             pic
         }
     },
-    methods:{
-        async picClickHandle(){
-            const data= await new Promise((resolve,reject)=>{
-                resolve({data:{code:1000,msg:'haha'}})
+    methods: {
+        async picClickHandle() {
+            const data = await new Promise((resolve, reject) => {
+                resolve({
+                    data: {
+                        code: 1000,
+                        msg: 'haha'
+                    }
+                })
             });
-            console.log('data',data);
+            console.log('data', data);
         }
     }
 }
 </script>
+
 <style lang="less" scoped>
-.app{
-    background:#ddd;
+.app {
+    background: #ddd;
+    color: blue;
 }
 </style>
